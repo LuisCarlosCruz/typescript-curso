95f3fc7708f389492a6cd24b2acb0450773a1041
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-01-12 09:31:10 -0300

    type boolean number e bigint

--- modulo-02/typeAnnotation/typeAnnotation.js
+++ modulo-02/typeAnnotation/typeAnnotation.js
@@ -15,3 +15,36 @@ function multiplicarNumero(num1, num2) {
 }
 ;
 multiplicarNumero(2, 5);
+// BOOLEAN
+// Boolean = Objeto
+// boolean = tipo primitivo
+let tarefaConcluida = true;
+let tarefaPendente = false;
+if (tarefaConcluida) {
+    console.log('Tarefa Concluída');
+}
+else {
+    console.log('Tarefa Pendente');
+}
+;
+// NUMBER & BIGINT
+let num = 14.0;
+console.log(num);
+// ex:
+let num1 = 23.0;
+let num2 = 0x78CF;
+let num3 = 0o577;
+let num4 = 0b110001;
+console.log('Number - Ponto Flutuante...: ', num1);
+console.log('Number - Hexadecimal...: ', num2);
+console.log('Number - Octal...: ', num3);
+console.log('Number - Binário...: ', num4);
+// ex.BIGINT
+let big1 = 9007199254740991n;
+let big2 = 9007199254740995n;
+let big3 = 0x20000000000003n;
+let big4 = 9007199254740995n;
+console.log('Bigint...: ', big1);
+console.log('Bigint - Binário...: ', big2);
+console.log('Bigint - Hexadecimal...: ', big3);
+console.log('Bigint - Octal...: ', big4);
